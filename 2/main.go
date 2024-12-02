7f813c645a8af4086468312e674b9c01b2a7c808
Johan Aanesen <johan.aanesen@gmail.com>
2024-12-02 08:50:35 +0100

    day 2 part 2

--- 2/main.go
+++ 2/main.go
@@ -27,36 +27,65 @@ func main() {
 	}
 
 	counter := 0
+	counter2 := 0
 
 	for i := 0; i < len(listOne); i++ {
 		tempArr := strings.Split(listOne[i], " ")
-		nrOne, _ := strconv.Atoi(tempArr[0])
-		nrTwo, _ := strconv.Atoi(tempArr[1])
 
-		safe := true
-
-		if nrOne < nrTwo { //increasing
-			for i := 0; i < len(tempArr)-1; i++ {
-				if !checkDif(tempArr[i], tempArr[i+1]) {
-					safe = false
-				}
-			}
-		} else { //decreasing
-			for i := len(tempArr) - 1; i > 0; i-- {
-				if !checkDif(tempArr[i], tempArr[i-1]) {
-					safe = false
-				}
-			}
-		}
-
+		safe, _ := partOne(tempArr)
 		if safe {
 			counter++
+		}
+
+		if !safe {
+			safesafe := false
+
+			for j := 0; j < len(tempArr); j++ {
+				s2 := make([]string, len(tempArr))
+				_ = copy(s2, tempArr)
+				safe2, _ := partOne(deleteElement(s2, j))
+
+				if safe2 {
+					safesafe = true
+					continue
+				}
+			}
+
+			if safesafe {
+				counter2++
+			}
 		}
 
 	}
 
 	fmt.Println("Part 1: ", counter)
+	fmt.Println("Part 2: ", counter2+counter)
 
+}
+
+func partOne(tempArr []string) (bool, int) {
+	nrOne, _ := strconv.Atoi(tempArr[0])
+	nrTwo, _ := strconv.Atoi(tempArr[len(tempArr)-1])
+
+	safe := true
+	unsafeCount := 0
+
+	if nrOne < nrTwo { //increasing
+		for i := 0; i < len(tempArr)-1; i++ {
+			if !checkDif(tempArr[i], tempArr[i+1]) {
+				safe = false
+				unsafeCount++
+			}
+		}
+	} else { //decreasing
+		for i := len(tempArr) - 1; i > 0; i-- {
+			if !checkDif(tempArr[i], tempArr[i-1]) {
+				safe = false
+				unsafeCount++
+			}
+		}
+	}
+	return safe, unsafeCount
 }
 
 func checkDif(one string, two string) bool {
@@ -70,4 +99,8 @@ func checkDif(one string, two string) bool {
 	}
 
 	return false
+}
+
+func deleteElement(s []string, i int) []string {
+	return append(s[:i], s[i+1:]...)
 }
