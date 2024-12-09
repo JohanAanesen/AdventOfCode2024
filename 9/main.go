ca43da72b0389919ac33891a64d05e88df36d03e
Johan Aanesen <johan.aanesen@gmail.com>
2024-12-09 11:32:55 +0100

    day 9 part 2 done, stupid mistake, forgot to break the for loop

--- 9/main.go
+++ 9/main.go
@@ -4,9 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+type DiskThing struct {
+	length int
+	value  string
+	moved  bool
+}
 
 func main() {
 
@@ -20,6 +27,8 @@ func main() {
 	r := bufio.NewScanner(b)
 
 	diskmap := make([]string, 0)
+
+	diskmap2 := make([]DiskThing, 0)
 	for r.Scan() {
 		line := r.Text()
 		input := strings.Split(line, "")
@@ -35,10 +44,14 @@ func main() {
 				for j := 0; j < inValue; j++ {
 					diskmap = append(diskmap, "free")
 				}
+
+				diskmap2 = append(diskmap2, DiskThing{value: "free", length: inValue, moved: false})
 			} else {
 				for j := 0; j < inValue; j++ {
 					diskmap = append(diskmap, strconv.Itoa(blockNameCounter))
 				}
+
+				diskmap2 = append(diskmap2, DiskThing{value: strconv.Itoa(blockNameCounter), length: inValue, moved: false})
 				blockNameCounter++
 			}
 
@@ -80,4 +93,71 @@ func main() {
 
 	fmt.Println("Part 1: ", checksum)
 
+	for i := len(diskmap2) - 1; i >= 0; i-- {
+
+		if diskmap2[i].value != "free" && !diskmap2[i].moved {
+
+			for j := 0; j < len(diskmap2); j++ {
+
+				if i <= j {
+					break
+				}
+
+				//finne første sted hvor det er en verdi som er free og lengde >= diskmap2[i].value
+				if diskmap2[j].value == "free" && diskmap2[j].length >= diskmap2[i].length {
+					//flytte verdien <-
+					if diskmap2[j].length == diskmap2[i].length {
+						diskmap2[j].value = diskmap2[i].value
+						diskmap2[j].moved = true
+
+						diskmap2[i].value = "free"
+						diskmap2[i].moved = false
+
+					} else {
+						diskmap2[j].value = diskmap2[i].value
+						lengdeDif := diskmap2[j].length - diskmap2[i].length
+						diskmap2[j].length = diskmap2[i].length
+						diskmap2[j].moved = true
+
+						diskmap2[i].value = "free" //skal stå
+						diskmap2[i].moved = false
+						//passe på empty spaces (sette inn free verdi med rest lengde midt i slice)
+						diskmap2 = slices.Insert(diskmap2, j+1, DiskThing{length: lengdeDif, value: "free", moved: false})
+					}
+
+					break
+
+				}
+
+			}
+
+		}
+
+	}
+
+	diskmap2Ferdig := make([]string, 0)
+	for i := 0; i < len(diskmap2); i++ {
+		for j := 0; j < diskmap2[i].length; j++ {
+			diskmap2Ferdig = append(diskmap2Ferdig, diskmap2[i].value)
+		}
+	}
+
+	checksum2 := 0
+
+	for i := 0; i < len(diskmap2Ferdig); i++ {
+		if diskmap2Ferdig[i] != "free" {
+			intVal, err := strconv.Atoi(diskmap2Ferdig[i])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			checksum2 += intVal * i
+		} else {
+			diskmap2Ferdig[i] = "."
+		}
+	}
+
+	//fmt.Println(diskmap2Ferdig)
+
+	fmt.Println("Part 2: ", checksum2)
 }
